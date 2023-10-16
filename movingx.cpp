162f1774f9ac63199bcfe7baa2220bcb17f516d0
girikanchan <akira.giri2001@gmail.com>
2023-10-17 00:39:56 +0530

    moving x at the end of the string

--- movingx.cpp
+++ movingx.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 #include<string.h>
 
-string movex(string s){
+std::string movex(std::string s){
     if(s.length()==0){
         return "";
     }
-    
+    char ch = s[0];
+    std::string ans = movex(s.substr(1));
+    if(ch == 'x'){
+        return (ans+ch);
+    }
+    return (ch+ans);
 }
 int main(){
-    cout<<movex("agxhjhfxxhdhfd");
+    std::cout<<movex("agxhjhfxxhdhfd");
     return 0;
 }

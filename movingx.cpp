a88d97c8007504495f7a904c1f5892031f7e918f
girikanchan <akira.giri2001@gmail.com>
2023-10-17 00:35:01 +0530

    move x to end of the string

---
+++ movingx.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+
+string movex(string s){
+    if(s.length()==0){
+        return "";
+    }
+    
+}
+int main(){
+    cout<<movex("agxhjhfxxhdhfd");
+    return 0;
+}

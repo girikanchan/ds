46c304897f88831b3691638190a2fd406ed85a74
girikanchan <akira.giri2001@gmail.com>
2023-11-02 21:31:59 +0530

    permutation in distinct integer

---
+++ permutationdistinct.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+// permutation of distinct integer
+
+// distinct integer means their will no repaeting values in integer.
+int main(){
+
+    
+    return 0;
+}
